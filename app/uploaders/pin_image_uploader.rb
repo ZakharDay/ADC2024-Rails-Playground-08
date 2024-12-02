e5c8069159fd78eebada84426b154c25a3014580
ZakharDay <zakhar.day@gmail.com>
2024-12-02 14:48:55 +0300

    Advansed views

--- app/uploaders/pin_image_uploader.rb
+++ app/uploaders/pin_image_uploader.rb
@@ -2,6 +2,8 @@ class PinImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
+  include CarrierWave::Vips
+  include CarrierWave::ImageOptimizer
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -23,29 +25,41 @@ class PinImageUploader < CarrierWave::Uploader::Base
 
   # Process files as they are uploaded:
   # process scale: [200, 300]
-  #
+  # 
   # def scale(width, height)
   #   # do something
   # end
 
   # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process resize_to_fit: [50, 50]
-  # end
+  version :thumb do
+    process resize_to_fill: [50, 50]
+  end
+
+  version :q70 do
+    process optimize: [{ quality: 70 }]
+  end
 
   # Add an allowlist of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_allowlist
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_allowlist
+    %w(jpg jpeg png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
-  # def filename
-  #   "something.jpg"
-  # end
+  def filename
+    "#{secure_token(10)}.#{file.extension}" if original_filename
+  end
 
   def asset_host
     return "http://localhost:3000"
   end
+
+  protected
+
+    def secure_token(length=16)
+      var = :"@#{mounted_as}_secure_token"
+      model.instance_variable_get(var) or model.instance_variable_set(var, SecureRandom.hex(length/2))
+    end
+
 end
