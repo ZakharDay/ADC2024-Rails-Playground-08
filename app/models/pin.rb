fed607f12cc5b0334087e6f82bc90f1b0ac29289
ZakharDay <zakhar.day@gmail.com>
2025-01-10 16:30:55 +0300

    2nd module

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,6 +2,7 @@ class Pin < ApplicationRecord
   has_many :comments
   has_many :likes, as: :likeable
   mount_uploader :pin_image, PinImageUploader
+  
   acts_as_taggable_on :tags
   acts_as_taggable_on :categories
 
